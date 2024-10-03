4785c3cbfc8e5af000174e2ee34810a469ede538
Hadassa <hadassa-mpmartins@educar.rs.gov.br>
2024-10-03 19:39:59 -0300

    Aula dia 27/09/2024

---
+++ src/Aula33/ExemploHashSet.java
@@ -0,0 +1,13 @@
+package Aula33;
+import java.util.HashSet;
+public class ExemploHashSet {
+    public static void main(String[] args) {
+        HashSet<String> compras = new HashSet<>();
+        System.out.println("Adicionando arroz a lista "+compras.add("Arroz"));
+        System.out.println("Adicionando pastel a lista "+compras.add("Pastel"));
+        System.out.println("Adicionando queijo a lista "+compras.add("Queijo"));
+        System.out.println("Adicionando chocolate a lista "+compras.add("Chocolate"));
+        System.out.println(compras);
+        System.out.println("Adicionando pastel a lista "+compras.add("Pastel"));
+    }
+}
